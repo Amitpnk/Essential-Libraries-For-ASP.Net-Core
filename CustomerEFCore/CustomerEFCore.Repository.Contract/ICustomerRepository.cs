0b5d98528585cd2bafa9e3f1e35538214e34e0ed
Amit P Naik <amit.naik8103@gmail.com>
2020-05-25 17:24:59 +0530

    repository

---
+++ CustomerEFCore/CustomerEFCore.Repository.Contract/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using CustomerEFCore.Domain;
+using System;
+using System.Threading.Tasks;
+
+namespace CustomerEFCore.Repository.Contract
+{
+    public interface ICustomerRepository
+    {
+        // General 
+        Task<bool> SaveChangesAsync();
+
+        // Customer
+        void AddCustomer(Customer customer);
+        void DeleteCustomer(Customer customer);
+        Task<Customer[]> GetAllCustomersAsync(bool includeOrders = false);
+        Task<Customer> GetCustomerAsync(string customerName, bool includeOrders = false);
+        Task<Customer[]> GetAllCustomersByCustomerName(string customerName, bool includeOrders = false);
+
+    }
+}
---
+++ CustomerEFCore/CustomerEFCore.Repository/CustomerRepository.cs
@@ -0,0 +1,63 @@
+﻿using CustomerEFCore.Data;
+using CustomerEFCore.Domain;
+using CustomerEFCore.Repository.Contract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerEFCore.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly CustomerContext _context;
+
+        public CustomerRepository(CustomerContext context)
+        {
+            _context = context;
+        }
+        public void AddCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+        }
+
+        public Task<Customer[]> GetAllCustomersAsync(bool includeOrders = false)
+        {
+            //IQueryable<Customer> query = _context.Customers
+            //   .Include(c => c.);
+
+            //if (includeOrders)
+            //{
+            //    query = query
+            //      .Include(c => c.Orders.Select(t => new {t.OrderId, t.OrderDate}));
+            //}
+
+            //// Order It
+            //query = query.OrderByDescending(c => c.EventDate);
+
+            //return await query.ToArrayAsync();
+
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer[]> GetAllCustomersByCustomerName(string customerName, bool includeOrders = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerAsync(string customerName, bool includeOrders = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+    }
+}
