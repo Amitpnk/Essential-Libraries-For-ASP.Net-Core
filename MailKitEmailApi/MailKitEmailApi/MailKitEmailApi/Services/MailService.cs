9bcde21b0ee9139921dc5d3b0b6f3342d253c59f
Amit P Naik <amit.naik8103@gmail.com>
2021-04-24 20:10:26 +0530

    feat: added mailkit

--- MailKitEmailApi/MailKitEmailApi/MailKitEmailApi/Services/MailService.cs
+++ MailKitEmailApi/MailKitEmailApi/MailKitEmailApi/Services/MailService.cs
@@ -1,10 +1,11 @@
-﻿using MailKitEmailApi.Models;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MailKitEmailApi.Models;
 using MailKitEmailApi.Settings;
 using Microsoft.Extensions.Options;
+using MimeKit;
 using System;
 using System.IO;
-using System.Net;
-using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace MailKitEmailApi.Services
@@ -16,16 +17,16 @@ namespace MailKitEmailApi.Services
         {
             _mailSettings = mailSettings.Value;
         }
-
         public async Task SendEmailAsync(MailRequest mailRequest)
         {
-            MailMessage message = new MailMessage();
-            SmtpClient smtp = new SmtpClient();
-            message.From = new MailAddress(_mailSettings.Mail, _mailSettings.DisplayName);
-            message.To.Add(new MailAddress(mailRequest.ToEmail));
-            message.Subject = mailRequest.Subject;
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            email.Subject = mailRequest.Subject;
+            var builder = new BodyBuilder();
             if (mailRequest.Attachments != null)
             {
+                byte[] fileBytes;
                 foreach (var file in mailRequest.Attachments)
                 {
                     if (file.Length > 0)
@@ -33,22 +34,19 @@ namespace MailKitEmailApi.Services
                         using (var ms = new MemoryStream())
                         {
                             file.CopyTo(ms);
-                            var fileBytes = ms.ToArray();
-                            Attachment att = new Attachment(new MemoryStream(fileBytes), file.FileName);
-                            message.Attachments.Add(att);
+                            fileBytes = ms.ToArray();
                         }
+                        builder.Attachments.Add(file.FileName, fileBytes, ContentType.Parse(file.ContentType));
                     }
                 }
             }
-            message.IsBodyHtml = false;
-            message.Body = mailRequest.Body;
-            smtp.Port = _mailSettings.Port;
-            smtp.Host = _mailSettings.Host;
-            smtp.EnableSsl = true;
-            smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new NetworkCredential(_mailSettings.Mail, _mailSettings.Password);
-            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-            await smtp.SendMailAsync(message);
+            builder.HtmlBody = mailRequest.Body;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
         }
 
         public async Task SendWelcomeEmailAsync(WelcomeRequest request)
@@ -58,20 +56,18 @@ namespace MailKitEmailApi.Services
             string MailText = str.ReadToEnd();
             str.Close();
             MailText = MailText.Replace("[username]", request.UserName).Replace("[email]", request.ToEmail);
-            MailMessage message = new MailMessage();
-            SmtpClient smtp = new SmtpClient();
-            message.From = new MailAddress(_mailSettings.Mail, _mailSettings.DisplayName);
-            message.To.Add(new MailAddress(request.ToEmail));
-            message.Subject = $"Welcome {request.UserName}";
-            message.IsBodyHtml = true;
-            message.Body = MailText;
-            smtp.Port = _mailSettings.Port;
-            smtp.Host = _mailSettings.Host;
-            smtp.EnableSsl = true;
-            smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new NetworkCredential(_mailSettings.Mail, _mailSettings.Password);
-            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-            await smtp.SendMailAsync(message);
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(request.ToEmail));
+            email.Subject = $"Welcome {request.UserName}";
+            var builder = new BodyBuilder();
+            builder.HtmlBody = MailText;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
         }
     }
 }
